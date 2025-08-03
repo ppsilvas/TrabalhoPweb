a07b89ba5603215ff85d3a2f776c95583c315e95
Prsonal Computer <jpepeu2005@gmail.com>
2025-08-03 01:47:38 -0300

    Tornar tela de cadastro funcional (POST request)

--- frontend/src/components/Cadastro/index.jsx
+++ frontend/src/components/Cadastro/index.jsx
@@ -1,11 +1,34 @@
-import axios from 'axios';
+import axios from '../../services/api';
 import {useState} from 'react';
 
 function Cadastro(){
 
-    //const [usuario, setUsuario] = useState({nome:'', cpf:'', email:'', senha:''})
+    const [usuario, setUsuario] = useState({
+        nome:'', 
+        cpf:'', 
+        email:'', 
+        senha:''
+    })
 
+    const handleInput = (event)=>{
+        setUsuario({...usuario, [event.target.name]: event.target.event})
+    }
+
+    function handleSubmit(event){
+        event.preventDefault()
+        axios.post('/posts', {usuario})
+        .then(response=>console.log(response))
+        .catch(err=>console.log(err))
+    }
+
+    //TODO: Estilizar
     return(
-        <h1>hii</h1>
+        <form onSubmit={handleSubmit}>
+            Nome: <input input type="text" onChange={handleInput} name="nome"></input>
+            CPF: <input input type="text" onChange={handleInput} name="cpf"></input>
+            E-mail: <input input type="text" onChange={handleInput} name="email"></input>
+            Senha: <input input type="text" onChange={handleInput} name="senha"></input>
+            <button>Cadastrar</button>
+        </form>
     );
 }export default Cadastro;
